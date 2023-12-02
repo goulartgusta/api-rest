78b47cea4e9c93157105e830f38817c59fc8e3e3
goulartgusta <goulartgusta9@gmail.com>
2023-12-02 11:19:27 -0300

    projeto iniciado

---
+++ src/main/java/br/com/anhembi/apiresttests/ApiRestTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.anhembi.apiresttests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/anhembi/apiresttests/ApiRestTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anhembi.apiresttests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
