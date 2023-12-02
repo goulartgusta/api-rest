9b3de8e5505c1e951b9c8c07045d06351e737bbb
goulartgusta <goulartgusta9@gmail.com>
2023-12-02 12:32:18 -0300

    classe user

---
+++ src/main/java/br/com/anhembi/apiresttests/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.anhembi.apiresttests.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
