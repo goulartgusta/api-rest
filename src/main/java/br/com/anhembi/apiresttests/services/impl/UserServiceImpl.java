c75c84c4c571ef9e62cbb4aff4d31dd8daf6cf64
goulartgusta <goulartgusta9@gmail.com>
2023-12-03 19:42:30 -0300

    tratamento de excecao para objeto nao encontrado

--- src/main/java/br/com/anhembi/apiresttests/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/anhembi/apiresttests/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.anhembi.apiresttests.services.impl;
 import br.com.anhembi.apiresttests.domain.Usuario;
 import br.com.anhembi.apiresttests.services.UserService;
 import br.com.anhembi.apiresttests.repositories.UserRepository;
+import br.com.anhembi.apiresttests.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public Usuario findById(Integer id) {
         Optional<Usuario> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 }
