65702fe7af04f4a95d4ecaeb6ceb3633d93ec952
goulartgusta <goulartgusta9@gmail.com>
2023-12-03 20:05:54 -0300

    classe user dto

---
+++ src/main/java/br/com/anhembi/apiresttests/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package br.com.anhembi.apiresttests.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
