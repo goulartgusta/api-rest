46cbba1a1d74a0aae2108db384e4b9f37c11173e
goulartgusta <goulartgusta9@gmail.com>
2023-12-03 23:02:21 -0300

    endpoint findAll para usuario

--- src/main/java/br/com/anhembi/apiresttests/services/UserService.java
+++ src/main/java/br/com/anhembi/apiresttests/services/UserService.java
@@ -1,7 +1,11 @@
 package br.com.anhembi.apiresttests.services;
 
 import br.com.anhembi.apiresttests.domain.Usuario;
+
+import java.util.List;
+
 public interface UserService {
 
     Usuario findById(Integer id);
+    List<Usuario> findAll();
 }
--- src/main/java/br/com/anhembi/apiresttests/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/anhembi/apiresttests/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.anhembi.apiresttests.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,9 @@ public class UserServiceImpl implements UserService {
     public Usuario findById(Integer id) {
         Optional<Usuario> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+    }
+
+    public List<Usuario> findAll(){
+        return repository.findAll();
     }
 }
