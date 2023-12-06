68e3d21cb914d9c65770d0221716e43dbb1f0cde
goulartgusta <goulartgusta9@gmail.com>
2023-12-05 22:49:11 -0300

    removendo bug

--- src/main/java/br/com/anhembi/apiresttests/domain/dto/UserDTO.java
+++ src/main/java/br/com/anhembi/apiresttests/domain/dto/UserDTO.java
@@ -3,10 +3,12 @@ package br.com.anhembi.apiresttests.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
